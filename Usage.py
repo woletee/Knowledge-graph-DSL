b0678dfbd9345ba472183dce8ab4d0f2c3023743
Liyew <118590747+woletee@users.noreply.github.com>
2024-04-25 19:33:40 +0900

    Create Usage.py

---
+++ Usage.py
@@ -0,0 +1,54 @@
+grid = {"train": [{"input": [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 8, 0, 8, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 8, 0, 0, 0, 0, 0, 0, 8, 0, 8, 0], [0, 0, 8, 0, 8, 0, 0, 0, 0, 0, 0, 8, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 0, 8, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 8, 0, 8, 0, 0, 0, 2, 0, 2, 0, 0], [0, 0, 0, 0, 8, 0, 0, 0, 0, 2, 0, 2, 0, 0], [0, 0, 0, 8, 0, 8, 0, 0, 0, 0, 2, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 2, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 2, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]], "output": [[8, 0, 8], [0, 8, 0], [8, 0, 8]]}, {"input": [[0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0], [0, 0, 4, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0], [0, 0, 0, 4, 4, 0, 0, 0, 1, 0, 0, 0, 0, 0], [0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 4], [0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 4, 0, 0], [0, 1, 0, 1, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0], [0, 1, 1, 1, 0, 0, 2, 0, 2, 0, 0, 0, 0, 0], [0, 0, 1, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 4, 0, 0], [0, 0, 4, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0], [0, 0, 0, 4, 4, 0, 0, 0, 0, 0, 0, 1, 0, 1], [0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0]], "output": [[4, 0, 0], [0, 4, 4], [4, 0, 0]]}, {"input": [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 8, 0, 0, 0, 0, 0, 6, 6, 0, 0, 0], [0, 0, 8, 8, 8, 0, 0, 0, 0, 6, 6, 0, 0, 0], [0, 0, 0, 8, 0, 0, 0, 0, 0, 0, 0, 6, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 8, 8, 8, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]], "output": [[0, 8, 0], [8, 8, 8], [0, 8, 0]]}], "test": [{"input": [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 3, 0, 3, 0, 0, 0, 0, 2, 0, 0, 0], [0, 0, 0, 0, 3, 3, 0, 0, 0, 2, 2, 2, 0, 0], [0, 0, 0, 3, 0, 3, 0, 0, 0, 2, 2, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0, 0], [0, 2, 0, 0, 0, 0, 8, 0, 8, 0, 0, 0, 0, 0], [2, 2, 2, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0, 0], [2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 2, 0, 0, 0, 3, 0, 3, 0, 0], [0, 0, 0, 0, 2, 2, 2, 0, 0, 0, 3, 3, 0, 0], [0, 0, 0, 0, 2, 2, 0, 0, 0, 3, 0, 3, 0, 0]], "output": [[0, 2, 0], [2, 2, 2], [2, 2, 0]]}]}
+grid_input_1 = grid["train"][0]["input"]
+grid_input_2 = grid["train"][1]["input"]
+grid_input_3 = grid["train"][2]["input"]
+grid_output_1 = grid["train"][0]["output"]
+grid_output_2 = grid["train"][0]["output"]
+grid_output_3 = grid["train"][0]["output"]
+
+node_list_input_1 = Make_NodeList(grid_input_1)
+edge_list_input_1 = create_edge_list
+temp_edge_list, tag_dsl1 = Make_edge_list(node_list_input_1, get_dsl1) ## iterate for every possible get_dsl
+edge_list_input_1 = Concat_edge_list(temp_edge_list, edge_list_input_1)
+
+temp_edge_list, tag_dsl2 = Make_edge_list(node_list_input_1, get_dsl2) 
+edge_list_input_1 = Concat_edge_list(temp_edge_list, edge_list_input_1)
+
+## repeat line 14 and 15 for every different get_dsl (pnode - pnode)
+
+node_list_input_1, edge_list_input_1 = Make_Onode (node_list_input_1, True, get_polar_dist, edge_list_input_1) 
+## construct secound, object layer
+
+temp_edge_list, tag_dsl3 = Make_edge_list(node_list_input_1, get_dsl3) 
+edge_list_input_1 = Concat_edge_list(temp_edge_list, edge_list_input_1)
+
+## repeat line 22 and 23 for every different get_dsl (onode - pnode / onode - onode)
+
+node_list_input_1, edge_list_input_1 = Make_Gnode(node_list_input_1, edge_list_input_1)
+## construct third, gnode layer
+
+temp_edge_list, tag_dsl3 = Make_edge_list(node_list_input_1, get_dsl3) 
+edge_list_input_1 = Concat_edge_list(temp_edge_list, edge_list_input_1)
+
+## repeat line 30 and 31 for every different get_dsl (gnode - pnode / gnode - onode)
+
+
+## repeat line 9 to 33 for grid_output_1
+node_list_output_1 = [...]
+edge_list_output_1 = [...]
+
+## concat input and output
+node_list_pair_1 = Concat_node_list(node_list_input_1, node_list_output_1)
+edge_list_pair_1 = Concat_edge_list(edge_list_input_1, edge_list_output_1)
+
+## construct fourth, vnode layer
+node_list_pair_1, edge_list_pair_1 = Make_Vnode(node_list_pair_1, edge_list_pair_1)
+
+temp_edge_list, tag_dsl3 = Make_edge_list(node_list_pair_1, get_dsl3) 
+edge_list_pair_1 = Concat_edge_list(temp_edge_list, edge_list_pair_1)
+
+## repeat line 47 and 48 for every different get_dsl (gnode(input) - gnode(output) / gnode(input) - onode(output) / onode(input) - gnode(output) / onode(input) - onode(output) / ...)
+## in this stage we draw every possible edge between input and output KG to make it one big KG
+
+## repeat line 9 to 51 for each 3 pairs from the task
+## and then concat all node_list_pair_n and edge_list_pair_n into one big list each 
